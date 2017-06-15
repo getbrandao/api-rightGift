a2e5c9783d92dff34be585fff181670435ca7778
Gustavo Brandão <gustavobrandao@bsd.com.br>
2017-06-15 14:08:32 -0300

    feature/#009-Authentication_SocialNetwork
    - Adjusted the tests were brokens.
    
    Signed-off-by: Gustavo Brandão <gustavobrandao@bsd.com.br>

--- spec/controllers/social_kinds/v1/index_action_spec.rb
+++ spec/controllers/social_kinds/v1/index_action_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe V1::SocialKindsController, '#index', type: [:controller, :social_kind], slow: true do
+  let(:user) {create(:login)}
   let(:social_kinds) {create(:social_kinds)}
 
   context "Accessing the index action" do
     before do
+      token_sign_in(user)
       @request.headers['Content-Type'] = 'application/vnd.api+json'
       @request.headers['Accept'] = 'application/vnd.api+json'
       get :index, format: :json
@@ -13,11 +15,11 @@ RSpec.describe V1::SocialKindsController, '#index', type: [:controller, :social_
     describe "should render :index template" do
       it 'returns successful 200 response' do
         expect(response).to be_success
-        # expect(response).to have_http_status(:ok)
         expect(response.content_type).to eq('application/vnd.api+json')
       end
       it "assigns a list of social_kind as @social_kinds" do
         create_list(:social_kinds, 5)
+        get :index, format: :json
         parsed_response = JSON.parse(response.body)
         expect(parsed_response["data"].length).to eq(5)
       end
